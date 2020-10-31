424bc5f4ceb0a7d686cbde171e01ef8e31707d97
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-31 12:03:38 +0100

    animation fixed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var color = ['spades','flowers','diamonds','hearts'];
 var values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
 var deck=[];
-var player = {
+var user = {
   name : 'player' ,
   points : 0 ,
   hand : [] ,
@@ -12,10 +12,11 @@ var cpu = {
   hand:[],
 }
 
-var timeUnit = 1000;
-var hiddenswitch = true;
+var timeUnit = 600;
 
 var cardSlideSound = new Audio ('resources/effects/cardslide.mp3')
+
+//DECK GENERATOR
 
 function deckCreation(){
   deck=[];
@@ -41,37 +42,17 @@ function shuffleDeck(){
     let index1= Math.floor(Math.random() * deck.length);
     let index2 = Math.floor(Math.random()* deck.length);
     let swappCard = deck[index1];
-
     deck[index1] = deck[index2];
     deck[index2] = swappCard;
   }
 }
 
-// function startGame(){
-//   for(i=0 ; i < 2;i++){
-//     player.hand.push(deck.pop());
-//     cpu.hand.push(deck.pop());
-//   }
-// }
-
-function calcolatePoints(){
-  player.points = 0;
-  cpu.points = 0;
-  for(i=0;i<player.hand.length;i++){
-    player.points += player.hand[i].num;
-  }
-  for(j=0;j<cpu.hand.length;j++){
-    cpu.points += cpu.hand[j].num;
-  }
-}
-
-
 function renderHand(string){
   if (string === "user"){
     var child = document.createElement('div');
     child.classList.add('playercard');
-    var index = player.hand.length - 1;
-    child.style.backgroundImage = "url(" + player.hand[index].img + ")";
+    var index = user.hand.length - 1;
+    child.style.backgroundImage = "url(" + user.hand[index].img + ")";
     document.getElementById('usersection').appendChild(child);
   }else if (string === "cpu"){
     var child = document.createElement('div')
@@ -89,38 +70,40 @@ function renderHand(string){
 };
 
 function firstHand(){
-      setTimeout(function(){
-        player.hand.push(deck.pop());
-        renderHand("user")
-        cardSlideSound.play()
-        document.getElementById('upper').style.webkitAnimation = "cardbackslidetoplayer .6s 2"
-      },timeUnit )
-      setTimeout(function(){
-        player.hand.push(deck.pop());
-        renderHand("user")
-        cardSlideSound.play()
-      }, 2 * timeUnit)
-        setTimeout(function(){
-          cpu.hand.push(deck.pop());
-          document.getElementById('upper').style.webkitAnimation = "cardbackslidetocpu .6s 2"
-          renderHand("cpu")
-          cardSlideSound.play()
-        },3 *  timeUnit)
-        setTimeout(function(){
-          cpu.hand.push(deck.pop());
-          renderHand("hidden")
-          cardSlideSound.play()
-        }, 4 * timeUnit)
+  setTimeout(function(){
+    giveCard(user,'user')
+    document.getElementById('upper').style.webkitAnimation = "cardbackslidetoplayer .6s 2"
+  },timeUnit )
+  setTimeout(function(){
+    giveCard(user,'user')
+  }, 2 * timeUnit)
+  setTimeout(function(){
+    giveCard(cpu,'cpu')
+    document.getElementById('upper').style.webkitAnimation = "cardbackslidetocpu .6s 2"
+    },3 *  timeUnit)
+  setTimeout(function(){
+    giveCard(cpu , 'hidden')
+  }, 4 * timeUnit)
 }
 
 
+function giveCard(arr,string){
+  arr.hand.push(deck.pop())
+  renderHand(string)
+  cardSlideSound.play();
+}
 
+
+function calcolatePoints(){
+  user.points = 0;
+  cpu.points = 0;
+  for(i=0;i<user.hand.length;i++){
+    user.points += user.hand[i].num;
+  }
+  for(j=0;j<cpu.hand.length;j++){
+    cpu.points += cpu.hand[j].num;
+  }
+}
 
 deckCreation();
 shuffleDeck();
-
-console.log(deck);
-console.log(cpu.hand,player.hand)
-calcolatePoints()
-console.log(player.points , " player point")
-console.log(cpu.points , " cpu point")
